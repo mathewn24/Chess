228869bd3b53583a7914978721873a69a80b94e0
mathewn24 <1mathewnoel@gmail.com>
2025-02-04 10:59:36 +0800

    fixed knight.cpp bug

--- knight.cpp
+++ knight.cpp
@@ -2,8 +2,6 @@
 #include "board.h"
 #include "knight.h"
 
-using namespace std;
-
 const int DY[] = {2, 2, 1, 1, -1, -1, -2, -2};
 const int DX[] = {1, -1, 2, -2, 2, -2, 1, -1};
 
@@ -15,10 +13,11 @@ Knight::Knight(Color color, int y, int x, Board* board) : Piece(color, y, x, boa
 bool Knight::isMoveValid(int y, int x) {
     Board* board = getBoard();
     for (int d = 0; d < 8; d++) {
-        if (y = getY() + DY[d] && x == getX() + DX[d]) {
+        if (y == getY() + DY[d] && x == getX() + DX[d]) {
             Piece* targ = board->get(getY() + DY[d], getX() + DX[d]);
-            if (targ == EMPTY || targ->getColor() != this->getColor())
+            if (targ == EMPTY || targ->getColor() != this->getColor()) {
                 return true;
+            }
         }
     }
     
